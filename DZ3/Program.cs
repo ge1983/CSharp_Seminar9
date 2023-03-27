eb3c7a340f83f272bfdf80dbcd69e96900f8b82b
Admin <ge1983@yandex.ru>
2023-03-27 17:12:57 +1000

    Добавлена задача 3 для ДЗ

---
+++ DZ3/Program.cs
@@ -0,0 +1,32 @@
+﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+
+int A(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0)
+    {
+        return A(m-1,1);
+    }
+    else
+    {
+        return A(m-1, A(m, n-1));
+    }
+}
+
+Console.WriteLine("Введите число m");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число n");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (m < 0 || n < 0)
+{
+    Console.WriteLine("Введеные числла должны быть неотрицательными");
+}
+else
+{
+    Console.WriteLine($"Результат функции Аккермана для чисел {m} и {n} равен {A(m,n)}");
+}
