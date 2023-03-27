2a7ebdb66a175ecf12dc2c9055662c01af4c57e5
Admin <ge1983@yandex.ru>
2023-03-27 16:49:33 +1000

    Добавлена задача 1 для ДЗ

---
+++ DZ1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+void PrintNumber(int N)
+{
+    if (N == 0)
+    {
+        return;
+    }
+    else
+    {
+        Console.Write($"{N} ");
+        PrintNumber(N-1);
+    }
+}
+
+Console.WriteLine("Введите число N");
+int N = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Полученный ряд");
+PrintNumber(N);
