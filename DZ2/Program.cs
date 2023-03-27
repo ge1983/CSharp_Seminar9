39114df0eaae5febc6c8e758c7762743ea00372f
Admin <ge1983@yandex.ru>
2023-03-27 17:00:00 +1000

    Добавлена задача 2 для ДЗ

---
+++ DZ2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+int SumMtoN(int M, int N)
+{
+    if (M == N)
+    {
+        return N;
+    }
+    else
+    {
+        return M + SumMtoN(M + 1, N);
+    }
+}
+
+Console.WriteLine("Введите число M");
+int M = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число N");
+int N = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Сумма от {M} до {N} равна {SumMtoN(M, N)}");
